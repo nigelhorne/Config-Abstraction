720c425841277ec0200934b39edbe6ba95608eff
Nigel Horne <njh@bandsman.co.uk>
2025-04-07 21:06:37 -0400

    Release 0.04

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Config::Abstraction - Configuration Abstraction Layer
 
 # VERSION
 
-Version 0.03
+Version 0.04
 
 # SYNOPSIS
 
--- lib/Config/Abstraction.pm
+++ lib/Config/Abstraction.pm
@@ -21,11 +21,11 @@ Config::Abstraction - Configuration Abstraction Layer
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
