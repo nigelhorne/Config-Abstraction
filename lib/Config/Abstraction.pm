85a3fe3b0aa43dad199d964a59eb1cf3ff5c3d89
Nigel Horne <njh@bandsman.co.uk>
2025-04-13 21:49:11 -0400

    Release 0.10

--- lib/Config/Abstraction.pm
+++ lib/Config/Abstraction.pm
@@ -17,11 +17,11 @@ Config::Abstraction - Configuration Abstraction Layer
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 =head1 SYNOPSIS
 
