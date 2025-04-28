474597093860e21886c53a5b196ad8b97b0c747b
Nigel Horne <njh@bandsman.co.uk>
2025-04-28 10:38:07 -0400

    More debugging

--- lib/Config/Abstraction.pm
+++ lib/Config/Abstraction.pm
@@ -327,8 +327,9 @@ sub _load_config
 				if((!defined($rc)) && $self->_load_driver('XML::PP')) {
 					my $xml_pp = XML::PP->new();
 ::diag(__LINE__);
+					$data = read_file($path);
 ::diag($data);
-					if(my $tree = $xml_pp->parse(read_file($path))) {
+					if(my $tree = $xml_pp->parse(\$data)) {
 use Data::Dumper;
 ::diag(Data::Dumper->new([$data])->Dump());
 						if($data = $xml_pp->collapse_structure($tree)) {
