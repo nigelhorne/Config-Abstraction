8af5a75ba65a24e8a22f3d2d65ffc37dd275cfa0
Nigel Horne <njh@bandsman.co.uk>
2025-06-18 14:35:35 -0400

    Also strip __ from end

--- lib/Config/Abstraction.pm
+++ lib/Config/Abstraction.pm
@@ -580,6 +580,7 @@ sub _load_config
 	# Merge ENV vars
 	my $prefix = $self->{env_prefix};
 	$prefix =~ s/_$//;
+	$prefix =~ s/__$//;
 	$prefix =~ s/::$//;
 	for my $key (keys %ENV) {
 		next unless $key =~ /^$self->{env_prefix}(.*)$/i;
